9667f4cd10ae3b41c8974e7b31ee1169e1998d02
DiegoFariasDF <diegofariaspl@gmail.com>
2024-06-28 00:43:48 -0300

    up falta alguns detalhes

--- header.js
+++ header.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('scroll', this.window.scrollY > 0)
 })
+
+
+
+const hamb = document.querySelector(".hamb");
+const nav = document.querySelector("#nav");
+
+hamb.addEventListener("click", () => nav.classList.toggle("ativo"));
